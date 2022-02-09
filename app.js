24ab642d329ecf19e8a678e584822a74fafdb73f
Ed Wong <31254224+edmondwong6279@users.noreply.github.com>
2022-02-09 12:25:40 +0000

    randomised username, timestamps and scroll window

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // Chat room where messages do not persist between clients,
 // you only see the messages that are sent/received since
 // you log in. Each user receives a random number string username.
+//
+// CLIENT:
+// - On connection, RECEIVES it's unique id [0, id]
+// - RECEIVES messages from server when other users (including themselves)
+//      send messages to the server [1, [id, message], time]
+// - SENDS messages that are in input field [id, message]
+//
+// SERVER:
+// - On connection, makes a new id and SENDS this to the client [0, id]
+// - RECEIVES message from the user [id, message], timestamps when it receives,
+//      SENDS back to all clients [1, [id, message], time].
 
 import express from "express";
 import WebSocket, { WebSocketServer } from "ws";
@@ -18,6 +29,8 @@ server.on("upgrade", (request, socket, head) => {
 
 // Makes a random string, checks against current clients
 // TODO this is not nice code, improve this.
+// ALSO if there are enough clients connecting at the same time,
+// this COULD give duplicate numbers. Async/await something here.
 wss.getUniqueID = () => {
     while (true) {
         let flag = false;
@@ -33,21 +46,26 @@ wss.getUniqueID = () => {
     }
 };
 
-// This is called between each new connection (I think).
 wss.on("connection", (ws) => {
     ws.id = wss.getUniqueID();
-    // does the client receive this?
-    ws.send(ws.id);
+    // number flag indicates it's sending the client it's id
+    ws.send(JSON.stringify([0, ws.id]));
 
+    // receives a message from the clients
     ws.on("message", (data) => {
-        console.log("---------------");
+        data = JSON.parse(data);
+        // NOTE data is a buffer object
+        // get the time when the server receives it and attach as time stamp
+        const timestamp = new Date();
         console.log(
-            `received: ${data}\nSending to ${wss.clients.size} clients.`
+            `received: ${data[1]}\nFrom: ${data[0]}\nAt Time: ${timestamp
+                .toTimeString()
+                .slice(0, 8)}\nSending to ${wss.clients.size} clients.`
         );
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
-                console.log(`Sending data to client ${client.id} now:`);
-                client.send(data);
+                console.log(`Sending data to client ${client.id} now.`);
+                client.send(JSON.stringify([1, data, timestamp]));
             }
         });
     });
